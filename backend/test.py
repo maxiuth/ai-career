e72c6eb84ffada1f9f2c17ded01b0773cb9cff7b
Uyen Tran <thtuyen1011@gmail.com>
2026-04-22 16:10:57 -0700

    APi call in test.py

--- backend/test.py
+++ backend/test.py
@@ -1,1 +1,19 @@
-print("Hello world")
+import requests
+
+def say_hi():
+    print("Hi there!")
+    
+def test_api():
+    results = requests.get(
+    "https://api.openwebninja.com/jsearch/search",
+    headers={
+      "x-api-key": "ak_gl43m34brq9m9b9vutakbmni61uyt79ot3esad9xz7pmi9z"
+    },
+    params={
+      "query": "software engineer"
+    })
+    
+    print(results.json()['data'])
+
+if __name__ == '__main__':
+    test_api()  
